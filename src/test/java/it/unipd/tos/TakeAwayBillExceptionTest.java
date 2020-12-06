11fa208106f6df4b471f8e9ddeaac57d57c9f42d
Massimo <maxdr64@gmail.com>
2020-12-06 11:32:52 +0100

    aggiunto takeawaybillexception

---
+++ src/test/java/it/unipd/tos/TakeAwayBillExceptionTest.java
@@ -0,0 +1,15 @@
+package it.unipd.tos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TakeAwayBillExceptionTest {
+
+    @Test
+    public void testTipExceptionWithMessage() {
+        TakeAwayBillException ex = new TakeAwayBillException("something wrong");
+        assertEquals("TakeAwayBillException Occurred: something wrong", ex.toString());
+    }
+
+}
